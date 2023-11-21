bc81b1db7733ba9e14165807c72220d7fe1a3678
Aurelian MELINTE <ame01@gmx.net>
2023-11-21 12:36:29 +0000

    Updated gittyup build script

---
+++ linux/build/gittyup/buildUbuntu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#
+# Modified Gittyup/pack/buildUbuntu.sh
+#
+
+pushd /opt
+sudo mkdir gittyup && sudo chmod a+rw gittyup
+popd
+
+sudo apt install build-essential libgl1-mesa-dev
+sudo apt install cmake
+sudo apt install libgit2-dev
+sudo apt install cmark
+sudo apt install git
+sudo apt install libssh2-1-dev
+sudo apt install openssl
+sudo apt install qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
+sudo apt install qttools5-dev
+sudo apt install ninja-build
+
+pushd ..
+git fetch
+git submodule init
+git submodule update
+git pull
+git checkout deps
+pushd dep/openssl/openssl/
+./config -fPIC
+make
+popd
+
+mkdir -vp build/release
+pushd build/release
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release  -DCMAKE_INSTALL_PREFIX=/opt/gittyup ../..
+cmake --build . --config Release --target install
+
