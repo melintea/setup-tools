3c289fee2be5e3b4f8f5565b82c64cf26d4315b2
Aurelian MELINTE <ame01@gmx.net>
2023-11-21 13:20:11 -0500

    Update buildUbuntu.sh

--- linux/build/gittyup/buildUbuntu.sh
+++ linux/build/gittyup/buildUbuntu.sh
@@ -4,9 +4,14 @@
 # Modified Gittyup/pack/buildUbuntu.sh
 #
 
+pwd
+
 pushd /opt
 sudo mkdir gittyup && sudo chmod a+rw gittyup
-popd
+popd #cwd
+
+git clone --depth 1 https://github.com/Murmele/Gittyup
+pushd Gittyup/pack
 
 sudo apt install build-essential libgl1-mesa-dev
 sudo apt install cmake
@@ -19,7 +24,7 @@ sudo apt install qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
 sudo apt install qttools5-dev
 sudo apt install ninja-build
 
-pushd ..
+pushd ..  #Gittyup
 git fetch
 git submodule init
 git submodule update
@@ -28,10 +33,12 @@ git checkout deps
 pushd dep/openssl/openssl/
 ./config -fPIC
 make
-popd
+popd  #Gittyup
 
 mkdir -vp build/release
 pushd build/release
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release  -DCMAKE_INSTALL_PREFIX=/opt/gittyup ../..
 cmake --build . --config Release --target install
+
+popd #cwd
 
