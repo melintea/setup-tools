33982084e4d2168e25c75fedbd8c40efc605dbdc
Aurelian MELINTE <ame01@gmx.net>
2024-05-27 07:34:24 -0400

    _tkrev.sh

---
+++ linux/tools/_tkrev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#  wget https://sourceforge.net/projects/tkcvs/files/tkrev_9.4.6.tar.gz/download
+#  mv download tkrev_9.4.6.tar.gz
+#  gunzip tkrev_9.4.6.tar.gz
+#  tar xvf tkrev_9.4.6.tar
+#  cd tkrev_9.4.6/
+#  sudo wish doinstall.tcl
+
+/usr/local/bin/tkrev
