57907e9f00dfd17b0430639ccbdb29a1a11e0ab0
Aurelian MELINTE <ame01@gmx.net>
2024-12-03 11:06:21 -0500

    _gitlog.sh

--- linux/tools/_gitlog.sh
+++ linux/tools/_gitlog.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-git log --graph --decorate --oneline $*
+#git log --graph --decorate --oneline $*
+git log  --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s' --date=short  
 
