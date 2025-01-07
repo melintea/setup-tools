3dd0c08092c314bf5dc1018c2250a4e9c4457eea
Aurelian MELINTE <ame01@gmx.net>
2025-01-07 00:03:08 +0000

    _update.sh

--- linux/tools/_update.sh
+++ linux/tools/_update.sh
@@ -8,18 +8,18 @@ sudo apt update && sudo apt upgrade
 
 sudo apt install \
 apt apt-utils aptitude synaptic debianutils dpkg \
+rpi-imager \
 firefox  thunderbird  seamonkey \
 keepass2 keepassxc \
 gdisk gparted \
 arp-scan bzip2 curl wget \
 git git-gui gitk mc tree meld xxdiff diffuse less nedit dos2unix vim jq rcs moreutils \
 ghostscript htop p7zip-full xz-utils zip \
-autotools-dev binutils-dev build-essentials cmake make ninja-build \
+autotools-dev binutils-dev build-essentials cmake make ninja-build checkinstall \
 bison flex g++ cgdb gdb clang clang-tidy lldb \
 linux-tools-common linux-tools-generic linux-tools-`uname -r` \
 google-perftools libgoogle-perftools-dev libbenchmark-dev graphviz \
 papi-tools papi-examples libpapi-dev \
 adb fastboot \
-rpi-imager \
 rstudio 
 
