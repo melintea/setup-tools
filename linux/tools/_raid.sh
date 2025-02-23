b65b478490efc4c6298a63049a89d1613f117583
Aurelian MELINTE <ame01@gmx.net>
2025-02-23 12:42:13 -0500

    _raid.sh

--- linux/tools/_raid.sh
+++ linux/tools/_raid.sh
@@ -4,6 +4,8 @@
 # Check raid status. Must be root
 # http://techie.org/Blog/2010/09/03/how-to-rebuild-intel-raid-isw-on-linux/
 #
+# Rebuild: dmraid -R isw_...
+#
 
 function run_cmd() {
     echo "=== $1"
@@ -12,7 +14,7 @@ function run_cmd() {
 
 run_cmd "dmsetup status"
 run_cmd "dmraid -r"
-run_cmd "dmraid -s -s"
+run_cmd "dmraid -s -s -v"
 run_cmd "dmraid -tvay"
 run_cmd "lsblk"
 
