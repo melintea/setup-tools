3be11b39564dd05dc6c78038451cfb8b957c8604
Aurelian MELINTE <ame01@gmx.net>
2025-03-15 17:53:15 -0400

    _ssdsync.sh

---
+++ linux/tools/_ssdsync.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#
+# Sync /home across drives
+#
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+
+mkdir /tmp/sda5
+mount -t auto /dev/sda5 /tmp/sda5
+if [ ! -d "/tmp/sda5/home" ]; then
+  echo "/tmp/sda5/home does not exist."
+  exit 1
+fi
+
+rsync -avz /home/ /tmp/sda5/home/
+
