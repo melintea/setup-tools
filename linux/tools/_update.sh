ad707902148165e7a9eeda1a3023140e9b4aa58f
Aurelian MELINTE <ame01@gmx.net>
2025-06-19 12:59:00 -0400

    _update.sh

--- linux/tools/_update.sh
+++ linux/tools/_update.sh
@@ -16,7 +16,7 @@ arp-scan bzip2 curl wget \
 git git-gui gitk mc tree meld xxdiff diffuse less nedit dos2unix vim jq rcs moreutils \
 ghostscript htop p7zip-full xz-utils zip \
 autotools-dev binutils-dev build-essentials cmake make ninja-build checkinstall \
-bison flex g++ cgdb gdb clang clang-tidy lldb \
+bison flex g++ cgdb gdb clang clang-tidy lldb rr \
 linux-tools-common linux-tools-generic linux-tools-`uname -r` \
 google-perftools libgoogle-perftools-dev libbenchmark-dev graphviz \
 papi-tools papi-examples libpapi-dev \
