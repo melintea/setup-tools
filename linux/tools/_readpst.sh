6af154bd20f03211d6fe1b0416d4afe20f7de8ed
Aurelian MELINTE <ame01@gmx.net>
2025-11-15 12:20:17 -0500

    _readpst.sh

---
+++ linux/tools/_readpst.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# https://www.linuxquestions.org/questions/linux-newbie-8/how-to-use-pst-utils-in-ubuntu-20-04-a-4175679631/
+#
+
+#readpst -M -b -e -o export_folder pst_archive.pst
+readpst -M -b -e -o . $1
+
