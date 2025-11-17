b8667822c1f410d5fd3aa7353bbd68098d15431f
Aurelian MELINTE <ame01@gmx.net>
2025-11-17 17:00:26 -0500

    _readpst.sh

--- linux/tools/_readpst.sh
+++ linux/tools/_readpst.sh
@@ -1,10 +1,20 @@
-#!/bin/bash
+#!/bin/bash 
 
 #
-# https://www.linuxquestions.org/questions/linux-newbie-8/how-to-use-pst-utils-in-ubuntu-20-04-a-4175679631/
+# https://www.flawlessrhetoric.com/Using-libpst-to-convert-PST-to-MBOX,-and-understanding-Thunderbird%27s-folder-structure
 #
+
+out=$(basename $1)
+out=${out%.*}
+mkdir $out
 
 #readpst -M -b -e -o export_folder pst_archive.pst
-readpst -b -u -o . $1
+readpst  -u -o $out $1
+#tree -apsDC .
+
+find $out -type d | grep -v '^'"$out"'$' | tac | xargs -d '\n' -I{} mv {} {}.sbd
+find $out -name mbox -type f | xargs -d '\n' -I{} echo '"{}" "{}"' | sed -e 's/\.sbd\/mbox"$/"/' | xargs -L 1 mv
+find $out -empty -type d | xargs -d '\n' rmdir
+find $out -type d | egrep '*.sbd' | sed 's/.\{4\}$//' | xargs -d '\n' touch
 tree -apsDC .
 
