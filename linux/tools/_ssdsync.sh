2d048a59439df8642acb764b8a570d7166964596
Aurelian MELINTE <ame01@gmx.net>
2025-12-03 12:19:49 -0500

    _ssdsync.sh

--- linux/tools/_ssdsync.sh
+++ linux/tools/_ssdsync.sh
@@ -4,10 +4,32 @@
 # Sync /home across drives
 #
 
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+
+function print()
+{
+    color=$1
+    text=$2
+    printf "${color}${text}${NC}\n"
+}
+
+
+# ------------------------------------------------------------------------
+
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit 1
 fi
+
+dmres=`/usr/sbin/dmraid -s -s`
+retval=$?
+if [[ $? != 0 || ! $dmres =~ "status : ok" ]]; then
+  print "${RED}" "FAIL WITH: ${retval}\n${dmres}\n"
+  exit 1
+fi
+
 
 mkdir /tmp/sda5
 mount -t auto /dev/sda5 /tmp/sda5
@@ -20,3 +42,7 @@ rsync -avzuxHAX /home/ /tmp/sda5/home/
 
 umount /tmp/sda5
 
+# timestamp last good run
+if [ -f "$0" ]; then
+  touch "$0"
+fi
