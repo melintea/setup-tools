237150293764d90000c27c7a98c1cc019252e29c
Aurelian MELINTE <ame01@gmx.net>
2026-02-09 09:53:11 -0500

    reorg gdb stuff

---
+++ linux/config/gdb/load_scripts.py
@@ -0,0 +1,17 @@
+import os
+import glob
+
+current_directory = os.getcwd()
+print(current_directory)
+
+# Directory to search for .gdbinit files
+# Use '.' for the current directory or specify an absolute path
+search_dir = os.environ['CUSTOMCFGROOT']
+pattern = os.path.join(search_dir, 'gdb', '_**')
+files = glob.glob(pattern, recursive=True)
+
+for file in files:
+    print("=== ", file)
+    # Use GDB's "source" command for each file found
+    gdb.execute(f'source {file}')
+
